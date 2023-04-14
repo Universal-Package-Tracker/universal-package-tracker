5f972be9e34e5590b301c894ff5069d66954c4c9
Jesus Ruescas Junior <jesusruescasjunior@gmail.com>
2023-04-14 15:01:22 -0300

    chore: init spring boot project :tada:

---
+++ src/main/java/com/tracker/up/universalpackagetracker/UniversalPackageTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.up.universalpackagetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalPackageTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalPackageTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracker/up/universalpackagetracker/UniversalPackageTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.up.universalpackagetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalPackageTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
